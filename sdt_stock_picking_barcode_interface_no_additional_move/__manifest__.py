0e8411a8eb3950744bc4dc7f51790ca208f0a147
Miftahussalam <miftah_salam@ymail.com>
2023-04-30 15:59:17 +0700

    [sdt_stock_picking_barcode_interface_no_additional_move] 14.0.1.0.0

---
+++ sdt_stock_picking_barcode_interface_no_additional_move/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0-standalone.html).
+{
+    "name": "Stock Picking Barcode Interface No Additional Move",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+
+    ],
+    "demo": [
+
+    ],
+    "images": [],
+}
